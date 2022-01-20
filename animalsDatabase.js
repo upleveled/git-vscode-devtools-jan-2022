e87a1475acb3671d84e4bde346022b070ff773bb
josehower <josehowerupn@gmail.com>
2022-01-20 11:55:49 +0100

    Add animal database

---
+++ animalsDatabase.js
@@ -0,0 +1,8 @@
+const animals = [
+  { animal: 'parrot', size: 's' },
+  { animal: 'dog', size: 's' },
+  { animal: 'cat', size: 's' },
+  { animal: 'elephant', size: 'b' },
+  { animal: 'tiger', size: 'm' },
+  { animal: 'lion', size: 'm' },
+];
